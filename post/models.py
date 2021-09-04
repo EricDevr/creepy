7cd69c881d7231a2ebe5323f6385e388157e13d7
EricDevr <acosta.eric40@gmail.com>
2021-09-04 04:27:22 -0400

    comments

--- post/models.py
+++ post/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Comments(models.Model):
+    id = models.AutoField(primary_key=True)
+    post = models.IntegerField()
+    user = models.CharField(max_length=50)
+    comment = models.TextField(max_length=2000)
+    date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.user
--- post/views.py
+++ post/views.py
@@ -1,11 +1,25 @@
 from django.http.response import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from home.models import Posts
+from .models import Comments
+from .forms import CommentForm
 # Create your views here.
 def post(request, idu, titleu):
     postg = Posts.objects.get(id=idu)
+    if request.method == "POST":
+        comment = CommentForm(request.POST)
+        com = Comments()
+        com.post = idu
+        com.user = comment.data["user"]
+        com.comment = comment.data["comment"]
+        if comment.is_valid():
+            com.save()
+    form = CommentForm()
+    comments = Comments.objects.filter(post=idu).order_by("-id")
     context = {
         'post': postg,
+        'comments': comments,
+        'form': form
     }
     return render(request, 'post.html', context)
 def voto(request, id_post, voto):
